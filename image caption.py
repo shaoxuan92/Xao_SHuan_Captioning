c04877307cf9ff2ba9a3839d8d8f2beb7ee50ae9
Xao_SHuan <shaoxuan92@163.com>
2017-12-23 15:16:02 +0800

    #first attempt from windows

--- image caption.py
+++ image caption.py
@@ -5,10 +5,12 @@ variety enhancement: learning method with two terms: one for correctness and one
 attention model: refer to paperweekly
 '''
 import numpy as np
-def load_data():
+dataset = {'flickr8k':(flickr8k.load_data,flickr8k.prepare_data) }
+def load_dataset(name):
+    return dataset[name][0],dataset[name][1]
 
-#train
-def train(
-        dataset = 'flickr8k'
-            ):
-    load_dataset(dataset)
+'''
+train
+'''
+def train(dataset = 'flickr8k'):
+    load_data,prepare_data=load_dataset(dataset)#we can see load_data,prepare_data as a function to call flick8k.py
