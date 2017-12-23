83cf0f67bfc95e4b08031b7aa701aae86217fced
xaoshuan <shaoxuan92@163.com>
2017-12-23 10:57:10 +0800

    first attempt to connect github and pycharm

--- image caption.py
+++ image caption.py
@@ -1,1 +1,9 @@
+'''
+models incorporated with structure, variety enchancement and new attention model
+structure:structured rnn + structured attentional network
+variety enhancement: learning method with two terms: one for correctness and one for variety
+attention model: refer to paperweekly
+'''
 import numpy as np
+#train
+def
