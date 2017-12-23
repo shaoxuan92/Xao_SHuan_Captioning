b90eb48f7054c30b0bf970cc706a161d563257c9
Xao_SHuan <shaoxuan92@163.com>
2017-12-23 15:31:06 +0800

    #first attempt from windows

--- flickr8k.py
+++ flickr8k.py
@@ -1,1 +1,13 @@
-def load_data():
+import cPickle as pkl
+def load_data(load_train=True,load_dev=True,load_test=True,path='./'):
+    ###############
+
+     #load data#
+
+    ###############
+
+    if load_train:
+        with open(path+'flicker_8k_align.train.pkl','rb') as f:
+    #here there is one question: two consecutive pkl.load(f) load two different things? like each column each load?
+            train_cap = pkl.load(f)
+            train_feat = pkl.load(f)
