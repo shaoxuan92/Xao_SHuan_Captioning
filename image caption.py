7a7685495264f326d54443ac50d56143a3e6179a
Xao_SHuan <shaoxuan92@163.com>
2017-12-27 20:45:36 +0800

    #first attempt from windows

--- image caption.py
+++ image caption.py
@@ -1,3 +1,4 @@
+#_*_coding:utf-8_*_
 '''
 models incorporated with structure, variety enchancement and new attention model
 structure:structured rnn + structured attentional network
@@ -20,8 +21,20 @@ def validate_options(options):
 def init_params(options):
 
     return params
-
-
+def _p(pp, name):
+    return '%s_%s' % (pp, name)
+def ortho_weight(ndim):
+    W = np.random.randn(ndim,ndim)
+    u,_,_=np.linalg.svd(W)
+    return u.astype('float32')
+def norm_weight(nin,nout=None, scale=0.01, ortho=True):
+    if nout is None:
+        nout = nin
+    if nin == nout and ortho:
+        W = ortho_weight(nin)
+    else:
+        W = scale * np.random.randn(nin,nout)
+    return W.astype('float32')
 ################################
 
 ### initialize theano shared ###
@@ -34,6 +47,71 @@ def init_tparams(params):
     for kk, pp in params.iteritems():
         tparams[kk]= theano.shared(params[kk],name=kk)#name defines the name of this theano variable
     return tparams
+
+''''
+Neural network layer definitions.
+The life-cycle of each of these layers is as follows
+    1) The param_init of the layer is called, which creates
+    the weights of the network.
+    2) The fprop is called which builds that part of the Theano graph
+    using the weights created in step 1). This automatically links
+    these variables to the graph.
+Each prefix is used like a key and should be unique
+to avoid naming conflicts when building the graph.
+
+'''
+layers = {'ff':('param_init_fflayer','fflayer'),
+          'lstm':('param_init_lstm','lstm_layer'),
+          'lstm_cond':('param_init_lstm_cond','lstm_layer_cond')}
+def get_layer(name):
+    fns = layers[name]
+    return(eval(fns[0]),eval(fns[1]))
+
+# feedforward layer: affine transformation + point-wise nonlinearity
+# make prefix-appended name
+
+
+def param_init_fflayer(options,params,prefix='ff',nin=None,nout=None):
+    if nin is None:
+        nin = options['dim_proj'] ######where is dim_proj
+    if nout is None:
+        nout = options['dim_proj']
+    params[_p(prefix,'W')] = norm_weight(nin,nout,scale=0.01)
+    params[_p(prefix,'b')] = np.zeros((nout,)).astype('float32')
+
+def fflayer(tparams,state_below,options,prefix='ronv',activ='lambda x:tensor.tanh(x)',**kwargs):#########state_below?????????
+    return eval(activ)(tensor.dot(state_below,tparams[_p(prefix,'W')])+tparams[_p(prefix,'b')])
+#conditional LSTM layer with Attention
+def param_init_lstm_cond(options,params,pre_fix='lstm_cond',nin=None,dim=None,dimctx=None):
+    if nin is None:###what is nin,dim,dimctx mean???????????????????????????????????? THINK IT AS Weight
+        nin = options['dim']
+    if dim is None:
+        dim = options['dim']
+    if dimctx is None:
+        dimctx = options['dim']
+# input to LSTM, similar to the above, we stack the matricies for compactness, do one
+# dot product, and use the slice function below to get the activations for each "gate"
+    W = np.concatenate([norm_weight(nin,dim)],
+                       [norm_weight(nin, dim)],
+                       [norm_weight(nin, dim)],
+                       [norm_weight(nin, dim)],axis=1)
+    params[_p(pre_fix,"W")] = W
+
+
+# LSTM to LSTM（可以理解成lstm是多层的）
+    U = np.concatenate([ortho_weight(dim),
+                           ortho_weight(dim),
+                           ortho_weight(dim),
+                           ortho_weight(dim)], axis=1)
+    params[_p(pre_fix,'U')] = U
+
+# bias to LSTM
+    params[_p(pre_fix,'b')] = np.zeros((4*dim,)).atype('float32')
+
+# context to LSTM
+    Wc = norm_weight(dimctx,dim*4)
+    params[_p(pre_fix,'Wc')] = Wc
+
 ################################
 
 ### build a training model ###
@@ -70,6 +148,44 @@ def build_model(tparams,options,sampling=True):
     trng = RandomStreams(12345)
     use_noise = theano.shared(np.float32(0.))
 
+#description: two dimensional??????
+    x = tensor.matrix('x',dtype='int64')
+    mask = tensor.matrix('mask',dtype='float32')
+#context: three dimensional??????
+    ctx = tensor.tensor3('ctx',dtype='float32')
+
+    n_timestep = x.shape[0]
+    n_samples = x.shape[1]
+
+# encoder
+    if options['lstm_encoder']:
+        pass
+    else:
+        ctx0 = ctx
+
+# initial state/cell
+    ctx_mean = ctx0.mean(1)
+    for lidx in xrange(1,options['n_layers_init']): # pay attention it starts at '1'
+        ctx_mean = get_layer('ff')[1](tparams, ctx_mean, options, prefix='ff_init_%d' % lidx, activ='rectifier')
+
+# [1] denotes that it only call the second function:fflayer
+    init_state = get_layer('ff')[1](tparams, ctx_mean, options, prefix='ff_state', activ='tanh')
+    init_memory = get_layer('ff')[1](tparams, ctx_mean, options, prefix='ff_memory', activ='tanh')
+#lstm decoder
+    attn_updates = []
+    proj, updates = get_layer('lstm_cond')[1](tparams,
+                                       emb,
+                                       options,
+                                       prefix='decoder',
+                                       mask=mask, context=ctx0,
+                                       one_step=False,
+                                       init_state=init_state,
+                                       init_memory=init_memory,
+                                       trng=trng,
+                                       use_noise=use_noise,
+                                       sampling=sampling))
+
+
 
 
 '''
@@ -77,7 +193,11 @@ train
 '''
 def train(dataset = 'flickr8k',
           max_epochs = 5000,
-          batch_size = 16):
+          batch_size = 16,
+          lstm_encoder=False, # if true,run biLSTM on input units
+          n_layers_init = 1,
+          dim = 1000
+        ):
     ##########################
 
     ### DATA PREPROCESSING ###
